f53f0a7c86831b18cbb2d128f594b819c9e608a6
Ab-Coder123 <abdoalmaestro123z@gmail.com>
2024-10-17 23:10:31 +0300

    fd

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Login from './modules/Login/Login';
 import Signup from './modules/SignUp/Signup';
-import ForgetPassword from './modules/Forgetpass/Forgetpass';
-import Verifycode from './modules/Verfycode/Verifycode';
-import ContactUs from './modules/ContactUs/ContactUs';
 // style
 import '../src/App.css'
-import Congratu from './modules/Congratulations/Congratu';
 import Paying from './modules/Paying/Paying';
 function App() {
   // تعريف الرواتر
@@ -25,23 +21,8 @@ function App() {
       path : '/modules/SignUp/Signup' , 
       element: <Signup /> ,
     } ,
-    {
-      path : '/modules/Forgetpass/Forgetpass' , 
-      element: <ForgetPassword /> ,
-    } ,
-    {
-      path : '/modules/Verfycode/Verifycode' , 
-      element: <Verifycode/> ,
-    },
-    {
-      path : '/modules/Congratulations/Congratu' , 
-      element: <Congratu/> ,
-    },
     
-    {
-      path : '/modules/ContactUs/ContactUs' , 
-      element: <ContactUs/> ,
-    },
+  
     {
       path : '/modules/Paying/Paying' , 
       element: <Paying/> ,
